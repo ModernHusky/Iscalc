92a929dafe62867f64b5170e5ee4b9d5c4a2b501
fyh <1913068672@qq.com>
2024-08-08 11:59:13 +0800

    add examples from kaoyan 4.3 section B

--- tests/action_test.py
+++ tests/action_test.py
@@ -3887,9 +3887,162 @@ class ActionTest(unittest.TestCase):
             replace substitution
             simplify
         done
+        
+        calculate INT x. 1 / sqrt(x^2 + 1)
+            substitute tan(u) for  x
+            rewrite tan(u)^2 + 1 to sec(u)^2
+            simplify
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. sqrt(x^2 - 9) / x for x > 3
+            substitute 3 * sec(u) for x
+            simplify
+            rewrite sqrt(9 * sec(u) ^ 2 - 9) to 3 * sqrt(sec(u)^2 - 1)
+            rewrite sec(u)^2 - 1 to tan(u)^2
+            simplify
+            rewrite tan(u)^2 to sec(u)^2 - 1
+            simplify
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1 / (x * sqrt(1-x^2)) for x > 0, x < 1
+            substitute sin(u) for x
+            rewrite 1 - sin(u)^2 to cos(u)^2
+            simplify
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1/(1+sqrt(2*x)) for x >= 0
+            substitute t for sqrt(2*x)
+            partial fraction decomposition
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1/(x^2*sqrt(1-x^2)) for x > 0, x < 1
+            substitute sin(u) for x
+            rewrite 1-sin(u)^2 to cos(u)^2
+            simplify
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        // page 181
+        
+        calculate INT x. 1/((x^2+1)*sqrt(1-x^2)) for x>0, x<1
+            substitute sin(u) for x
+            rewrite 1-sin(u)^2 to cos(u)^2
+            simplify
+            rewrite 1/(sin(u)^2+1) to (1/cos(u))^2 / ((sin(u)/cos(u))^2 + (1/cos(u))^2)
+            rewrite 1/cos(u) to sec(u)
+            rewrite 1/cos(u) to sec(u)
+            rewrite sin(u)/cos(u) to tan(u)
+            rewrite sec(u)^2 to 1+tan(u)^2 (at 2)
+            simplify
+            substitute v for tan(u)
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1/((x^2+1)*sqrt(x^2-1)) for x > 1
+            substitute sec(u) for x
+            rewrite sec(u)^2-1 to tan(u)^2
+            simplify
+            rewrite sec(u) to 1/cos(u)
+            rewrite sec(u) to 1/cos(u)
+            rewrite 1 / cos(u) / ((1 / cos(u)) ^ 2 + 1) to cos(u) / (1+cos(u)^2)
+            rewrite cos(u)^2 to 1-sin(u)^2
+            substitute v for sin(u)
+            substitute sqrt(2)*w for v
+            simplify
+            partial fraction decomposition
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1/(x+sqrt(1-x^2)) for x > 0, x < 1
+            substitute sin(u) for x
+            rewrite 1-sin(u)^2 to cos(u)^2
+            simplify
+            rewrite cos(u) / (cos(u) + sin(u)) to 1/2 * ((cos(u) + sin(u) + cos(u)-sin(u))/ (cos(u) + sin(u)))
+            rewrite (cos(u) + sin(u) + cos(u)-sin(u))/ (cos(u) + sin(u)) to 1 + (cos(u)-sin(u)) / (cos(u)+sin(u))
+            simplify
+            apply integral identity
+            substitute v for cos(u)+sin(u)
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. (x^3+1) / (x^2+1)^2
+            substitute tan(u) for x
+            rewrite tan(u)^2+1 to  sec(u)^2
+            simplify
+            rewrite tan(u) to sin(u)/cos(u)
+            rewrite sec(u) to 1/cos(u)
+            expand polynomial
+            simplify
+            rewrite sin(u)^3 to sin(u)^2 * sin(u)
+            rewrite sin(u)^2 to 1 - cos(u)^2
+            substitute v for cos(u)
+            simplify
+            expand polynomial
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1 / (sqrt(x)+x^(1/4)) for x > 0
+            substitute u for x^(1/4)
+            simplify
+            rewrite u^2+u to u*(u+1)
+            simplify
+            partial fraction decomposition
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. sqrt((1-x) / (x^2*(1+x))) for x > 0, x < 1
+            rewrite (1-x) / (x^2*(1+x)) to ((1-x)*(1+x)) / (x^2 * (1+x)^2)
+            rewrite (1-x)*(1+x) to 1-x^2
+            rewrite sqrt((1-x^2) / (x ^ 2 * (1 + x) ^ 2)) to sqrt(1-x^2) / (x*(1+x))
+            substitute sin(u) for x
+            rewrite 1-sin(u)^2 to cos(u)^2
+            simplify
+            rewrite cos(u)^2 to 1-sin(u)^2
+            rewrite 1-sin(u)^2 to (1+sin(u))*(1-sin(u))
+            simplify
+            expand polynomial
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        calculate INT x. 1 / (exp(x/2) + exp(x))
+            substitute u for exp(x/2)
+            simplify
+            partial fraction decomposition
+            apply integral identity
+            replace substitution
+            simplify
+        done
+        
+        // calculate INT x. exp(atan(x))
+        
         """
         self.check_actions("base", "PostgraduateIndefinitePart2SectionB", actions)
-
 
 if __name__ == "__main__":
     unittest.main()
