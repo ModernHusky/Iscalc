d243a567318dbd84f25d6826a91fef86b8d66028
seaway <1316837928@qq.com>
2025-05-02 02:41:28 +0800

    Remove path. -_-

--- tests/action_test.py
+++ tests/action_test.py
@@ -7,8 +7,6 @@ from integral import compstate
 from integral import state
 from integral import parser
 from integral import expr
-import os
-os.chdir("E:\Agraduatelife\learn-git\iscalc")
 
 class ActionTest(unittest.TestCase):
     def check_actions(self, base_file: str, current_file: str, actions: str,
