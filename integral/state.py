8a06c68cee9646643a4062df80819a3a9b5bc7ae
fyh <1913068672@qq.com>
2025-07-01 03:06:16 +0800

    fix

--- integral/state.py
+++ integral/state.py
@@ -198,6 +198,10 @@ class CalculateState(State):
         elif isinstance(action, RHSAction):
             return self.past.process_action(action)
 
+        elif isinstance(action, SubgoalAction):
+            subgoal = self.calc.add_subgoal(action.name, action.expr, action.conditions)
+            return ProveState(self, subgoal)
+
         # Other cases are invalid
         else:
             raise StateException(
