90d2449c964f2c11ac90571ddce63054cf2c7579
fyh <1913068672@qq.com>
2025-09-05 14:41:30 +0800

    remove files

--- theories/probability_integral1.thy
+++
@@ -1,2 +0,0 @@
-imports interesting2
-prove (INT x:[-oo,oo]. exp(-(x ^ 2) / 2)) = sqrt(2 * pi)
--- theories/probability_integral2.thy
+++
@@ -1,3 +0,0 @@
-imports interesting2
-prove (INT x:[-oo,oo]. exp(-(x ^ 2) / 2)) = sqrt(2 * pi)
-prove (INT x:[0,oo]. exp(-(x ^ 2) / 2)) = sqrt(2) * sqrt(pi) / 2
